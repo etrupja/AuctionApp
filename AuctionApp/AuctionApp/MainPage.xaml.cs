e566597da370b9217c4b4a5fea7d02fd649c5e5c
ERVIS-PC\Ervis <ervistrupja@outlook.com>
2018-11-24 19:39:25 +0100

    Button set up

--- AuctionApp/AuctionApp/MainPage.xaml.cs
+++ AuctionApp/AuctionApp/MainPage.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.WindowsAzure.MobileServices;
 using Xamarin.Forms;
 
 namespace AuctionApp
@@ -12,6 +13,16 @@ namespace AuctionApp
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            message.Text = "Loading items...";
+
+            MobileServiceClient client = new MobileServiceClient("http://localhost:30341/");
+            var items = await client.GetTable<TodoItem>().ReadAsync();
+            var item = items.First();
+            message.Text = item.Text;
         }
     }
 }
---
+++ AuctionApp/AuctionApp/TodoItem.cs
@@ -0,0 +1,10 @@
+﻿namespace AuctionApp
+{
+    public class TodoItem
+    {
+        public string Id { get; set; }
+        public string Text { get; set; }
+
+        public bool Complete { get; set; }
+    }
+}
